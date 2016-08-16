9958f62113ab87112a97003e5153a563fd8eff9f
alexk-restoin <a.kryzhanovskyy@resto-in.com>
2016-08-16 18:12:51 +0200

    clean and images tasks

--- tools/tasks/dev.ts
+++ tools/tasks/dev.ts
@@ -1,13 +1,14 @@
 import * as sequence from 'run-sequence';
 var $ = global;
 
-$.gulp.task('dev', 'Create development distribution app', (cb:any)=> {
+$.gulp.task('dev', 'Create development distribution app', ['clean'], (cb:any)=> {
     cb = cb || function() {};
     sequence(
         [
             'icons',
             'mocks',
-            'favicon'
+            'favicon',
+            'images'
         ],
         cb
     );
